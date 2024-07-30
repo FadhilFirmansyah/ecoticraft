0ef1a1b861e3f450470631a926af87bf19697252
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-07-30 15:06:07 +0700

    change img

--- js/library/btn_animation.js
+++ js/library/btn_animation.js
@@ -1,13 +1,16 @@
-let btn = document.getElementById("flow_btn");
+let backbtn = document.getElementById("flow_btn");
 let maxVh = window.innerHeight; // ukuran tinggi layar
 
 window.addEventListener('scroll', function () {
     let Ylevel = window.scrollY;
 
     if(Ylevel > maxVh*0.75){
-        btn.style.bottom = "5%";
+        backbtn.style.bottom = "5%";
     } else {
-        btn.style.bottom = "-10%";
+        backbtn.style.bottom = "-10%";
     }
 
 })
+
+let socmed = document.getElementById("socmed");
+
