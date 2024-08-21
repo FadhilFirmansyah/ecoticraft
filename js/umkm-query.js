cacaea7f46f909e9005e7ee85d4534602bda757a
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-08-21 21:50:02 +0700

    add: umkm

---
+++ js/umkm-query.js
@@ -0,0 +1,30 @@
+
+let umkm_wraps = document.querySelector(".products-wrap");
+
+
+fetch('https://haydar-hilmy.github.io/ecoticraft/umkm-data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(datas => {
+        datas.forEach(data => {
+            let card = `
+                        <div class="card-product">
+                        <div style="background-image: url('assets/umkm/${data.gambar}');" class="img-product ${data.gambar == '' ? 'no-img-product' : ''}">${data.gambar == '' ? 'gambar tidak tersedia :(' : ''}</div>
+                        <div class="title-product">
+                        <span>
+                        <h4>${data.judul}</h4>
+                        <h5>${data.alamat_lengkap}</h5>
+                        <a href="https://wa.me/${data.no_telp}" target="_blank">${data.no_telp}</a>
+                        </span>
+                        </div>
+                        </div>`;
+            umkm_wraps.innerHTML += card;
+        });
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
