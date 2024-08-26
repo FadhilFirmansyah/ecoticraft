471bdf1d5e9fcb5275681048ac1fa9bfa4ed7765
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-08-26 21:12:38 +0700

    fix url

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 // $root  = "http://".$_SERVER['HTTP_HOST'];
 // $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$config['base_url'] = 'http://localhost/project/ECOTICRAFT/ci3/ecoticraft/';
+$config['base_url'] = 'https://ecoticraft.org/';
 
 /*
 |--------------------------------------------------------------------------
